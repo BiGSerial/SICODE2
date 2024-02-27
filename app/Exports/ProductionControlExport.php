b4bb94c7b257ac2b39e997895abf6b09695deef1
Will Oliveira <woliveiravix@gmail.com>
2024-02-27 11:20:20 -0300

    Fix Export Porductions Stack

--- app/Exports/ProductionControlExport.php
+++ app/Exports/ProductionControlExport.php
@@ -2,12 +2,16 @@
 
 namespace App\Exports;
 
+use App\Models\Edp_depc\City;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithProperties;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 
-class ProductionControlExport implements FromView, WithProperties
+class ProductionControlExport implements FromView, WithProperties, WithEvents
 {
     use Exportable;
 
@@ -37,10 +41,33 @@ class ProductionControlExport implements FromView, WithProperties
         ];
     }
 
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                // Define o estilo para a primeira linha
+                $event->sheet->getStyle('A1:P1')->applyFromArray([
+                    'font' => [
+                        'bold' => true,
+                        'color' => ['rgb' => 'FFFFFF'], // Cor do texto (branco)
+                    ],
+                    'fill' => [
+                        'fillType' => Fill::FILL_SOLID,
+                        'startColor' => ['rgb' => '0000FF'], // Cor de fundo (azul)
+                    ],
+                ]);
+
+                $event->sheet->autoSize();
+            },
+        ];
+    }
+
     public function view(): View
     {
+
         return view('exports.productionsControl', [
-            'lists' => $this->data
+            'lists' => $this->data,
+            'cities' => City::get()
         ]);
     }
 }
