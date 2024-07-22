10b9d3c00b1fdfe378c3faca29b7b063410a5396
Will Oliveira <woliveiravix@gmail.com>
2024-07-22 11:34:03 -0300

    Updating

--- limpar.bat
+++ limpar.bat
@@ -11,4 +11,3 @@ pause
 mysql -u root -p -h localhost -e "SET FOREIGN_KEY_CHECKS=0; TRUNCATE TABLE files; TRUNCATE TABLE files_form; TRUNCATE TABLE comments; TRUNCATE TABLE comment_reclaim; TRUNCATE TABLE comment_viability; TRUNCATE TABLE file_production; TRUNCATE TABLE forms; TRUNCATE TABLE reclaims; TRUNCATE TABLE reclaim_viability; TRUNCATE TABLE viabilities; SET FOREIGN_KEY_CHECKS=1" sicode2
 
 @echo off
-p
