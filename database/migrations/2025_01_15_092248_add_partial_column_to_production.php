c6d7e769a426f26018731df210ca14b9a5bdd450
Will Oliveira <woliveiravix@gmail.com>
2025-02-18 16:33:32 -0300

    quick_save

--- database/migrations/2025_01_15_092248_add_partial_column_to_production.php
+++ database/migrations/2025_01_15_092248_add_partial_column_to_production.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('productions', function (Blueprint $table) {
-            $table->boolean('partial')->nullable()->default(false);
+            $table->boolean('partial')->default(false);
         });
     }
 
